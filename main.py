e27e9de8d68f46fd015da2c932d313bb359d9d7a
Yosef <josephaschoen@gmail.com>
2024-06-19 23:02:12 +0300

    Redoing Project And Extending its scope

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-import scraper
 import json
 import boto3
+import sys
+import scraper
+
+
+page_num = int(sys.argv[1])
+num_pages = int(sys.argv[2])
 
 s3 = boto3.client('s3')
 bucket_name = 'scraped-data-raw'
 
-for i in range(1, 450):
+for i in range(page_num, page_num+num_pages+1):
     for j in range(15):
         try:
             data = scraper.main(i, j)
@@ -18,7 +23,7 @@ for i in range(1, 450):
         except:
             print(f'failed on page: {i:03d} collection: {j:03d}')
             data = 'failed to scrape'
-            object_key = f'{i:03d}/{j:03d}.text'
+            object_key = f'failed/{i:03d}/{j:03d}.text'
             s3.put_object(Bucket=bucket_name, Key=object_key, Body=data)
 
 
