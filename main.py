98ebc4dd3c7095a91940cfe26fcf406bd214690c
Yosef <josephaschoen@gmail.com>
2024-06-19 22:27:40 +0300

    Redoing Project And Extending its scope

--- main.py
+++ main.py
@@ -5,21 +5,20 @@ import boto3
 s3 = boto3.client('s3')
 bucket_name = 'scraped-data-raw'
 
-for i in range(50, 51):
+for i in range():
     for j in range(5, 15):
         try:
             data = scraper.main(i, j)
             data = json.dumps(data)
-            print(data)
 
-            object_key = f'{i}/{j}.json'
+            object_key = f'{i:03d}/{j:03d}.json'
             # Upload to S3
             s3.put_object(Bucket=bucket_name, Key=object_key, Body=data)
-
+            print(f'succeeded on  page: {i:03d} collection: {j:03d}')
         except:
-            print(f'failed on page: {i} collection: {j}')
+            print(f'failed on page: {i:03d} collection: {j:03d}')
             data = 'failed to scrape'
-            object_key = f'{i}/{j}.text'
+            object_key = f'{i:03d}/{j:03d}.text'
             s3.put_object(Bucket=bucket_name, Key=object_key, Body=data)
 
 
