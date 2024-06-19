3ce7f73e7e5c2b776c27ecb2e0bc091a4d593ad8
Yosef <josephaschoen@gmail.com>
2024-06-19 22:03:10 +0300

    Redoing Project And Extending its scope

--- main.py
+++ main.py
@@ -12,13 +12,13 @@ for i in range(50, 51):
             data = json.dumps(data)
             print(data)
 
-            object_key = f'{i}/{j}.txt'
+            object_key = f'{i}/{j}.json'
             # Upload to S3
             s3.put_object(Bucket=bucket_name, Key=object_key, Body=data)
 
         except:
             print(f'failed on page: {i} collection: {j}')
-            data = f'page: {i}, collection: {j}'
-            object_key = 'fail.txt'
+            data = 'failed to scrape'
+            object_key = f'{i}/{j}.text'
             s3.put_object(Bucket=bucket_name, Key=object_key, Body=data)
 
