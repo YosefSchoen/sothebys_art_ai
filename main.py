6bd860a11438d70230031c5417e019f4081d728f
Yosef <josephaschoen@gmail.com>
2024-07-31 22:33:02 +0300

    Redoing Project And Extending its scope

--- main.py
+++ main.py
@@ -3,27 +3,24 @@ import boto3
 import sys
 import scraper
 
-
 page_num = int(sys.argv[1])
-num_pages = int(sys.argv[2])
-
+collection_num = int(sys.argv[2])
 s3 = boto3.client('s3')
+
 bucket_name = 'scraped-data-raw'
+try:
+    data = scraper.main(page_num, collection_num)
+    data = json.dumps(data)
 
-for i in range(page_num, page_num+num_pages+1):
-    for j in range(15):
-        try:
-            data = scraper.main(i, j)
-            data = json.dumps(data)
+    object_key = f'{page_num:03d}/{collection_num+1:03d}.json'
+    # Upload to S3
+    s3.put_object(Bucket=bucket_name, Key=object_key, Body=data)
+    print(f'succeeded on  page: {page_num:03d} collection: {collection_num + 1:03d}')
 
-            object_key = f'{i:03d}/{j:03d}.json'
-            # Upload to S3
-            s3.put_object(Bucket=bucket_name, Key=object_key, Body=data)
-            print(f'succeeded on  page: {i:03d} collection: {j:03d}')
-        except:
-            print(f'failed on page: {i:03d} collection: {j:03d}')
-            data = 'failed to scrape'
-            object_key = f'failed/{i:03d}/{j:03d}.text'
-            s3.put_object(Bucket=bucket_name, Key=object_key, Body=data)
+except:
+    print(f'failed on page: {page_num:03d} collection: {collection_num + 1:03d}')
+    data = 'failed to scrape'
+    object_key = f'failed/{page_num:03d}/{collection_num + 1:03d}.text'
+    s3.put_object(Bucket=bucket_name, Key=object_key, Body=data)
 
 
